db4bae3714bfce2ae73510cd9854a024afbb7d13
Niicoph <niicoph@gmail.com>
2023-10-24 21:05:42 -0300

    Front v2

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Gimnasio</title>
+</head>
+<body>
+    <main>
+        <h1>Gimnasio X</h1>
+        <div class="container">
+            <div class="box" style="border:3px solid green">
+                <p>Agregar Cliente</p>
+                <img src="GestionCliente/img/add.png" alt="add image">
+            </div>
+            <div class="box" style="border:3px solid blue">
+                <p>Buscar Cliente</p>
+                <img src="GestionCliente/img/search.png" alt="search image">
+            </div>
+            <div class="box" style="border:3px solid red">
+                <p>Eliminar Cliente</p>
+                <img src="GestionCliente/img/close.png" alt="delete image">
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+
